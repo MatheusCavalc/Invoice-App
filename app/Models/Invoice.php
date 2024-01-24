29c1014841e7d6f82fc814964aca2245b58afbdf
Matheus Cavalcante <matheuscavalcs@gmail.com>
2024-01-24 20:21:26 -0300

    migrations and models

---
+++ app/Models/Invoice.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['client_id', 'number', 'issue_date', 'total'];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(InvoiceItem::class);
+    }
+}
---
+++ database/migrations/2024_01_24_224055_create_invoices_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained();
+            $table->string('number');
+            $table->date('issue_date');
+            $table->decimal('total', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
